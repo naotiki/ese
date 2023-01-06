d2c57366570c95334f3a22a4a2e5839d077e5424
Naotiki <j22413@inc.kisarazu.ac.jp>
2023-01-06 23:53:04 +0900

    Add:File/Path解決

--- src/main/kotlin/File.kt
+++ src/main/kotlin/File.kt
@@ -1,15 +1,111 @@
+/**
+ * 仮想的ファイルの抽象クラス
+ * ディレクトリもファイルとする。
+ *  @param name ファイルの名前
+ *  @param parent 親ディレクトリ、ルートの場合はnull
+ *  @param attribute 属性 [FileAttribute]をとる
+ */
+abstract class File(
+    var name: String,
+    var parent: Directory?,
+    var attribute: Int = FileAttribute.None
+) {
+    fun getFullPath(): Path {
+        val path = mutableListOf<String>()
+        var f: File? = this
+        while (f?.parent != null) {
+            path.add(f.name)
+            f = f.parent
+        }
+        return Path(path.reversed().joinToString("/", "/"))
+    }
+}
 
-abstract class File
-    (var name: String, var attribute: Int = Attribute.None)
-{
+class TextFile(
+    name: String, parent: Directory?, content: String
+) : File(name, parent) {
+     var content = content
+         private set
 
 }
 
-enum class FileType{
+
+open class Directory(name: String, parent: Directory?) : File(name, parent = parent) {
+    protected open var _children: MutableMap<String, File> = mutableMapOf()
+    val children get() = _children.toMap()
+
+
+    fun addChildren(vararg childDir: File) {
+        _children.putAll(childDir.associateBy { it.name })
+    }
+}
+
+// 進捗状況に応じて中身が変わるディレクトリ
+class DynamicDirectory(name: String, parent: Directory?) :Directory(name, parent){
+    init {
+        EventManager.addEventListener {
+
+        }
+    }
+    override var _children: MutableMap<String, File>
+        get() = super._children
+        set(value) {}
+}
+
+object EventManager{
+    inline fun addEventListener(block:()->Unit){
+
+    }
+}
+
+
+//初期状態
+
+object FileManager {
+    lateinit var homeDir: Directory
+
+    init {
+        rootDir {
+            dir("bin") {
+
+            }
+            dir("home") {
+                homeDir = dir(userName) {
+                    file(
+                        "Readme.txt",
+                        """
+                        やぁみんな俺だ！
+                        このファイルを開いてくれたんだな！
+                        """.trimIndent()
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+val root = Directory("", null)
+inline fun rootDir(block: Directory.() -> Unit) {
+    root.block()
+}
+
+fun Directory.dir(name: String, block: Directory.() -> Unit): Directory {
+    return Directory(name, this).also {
+        addChildren(it)
+        it.block()
+    }
+}
+
+fun Directory.file(name: String, content: String) = addChildren(TextFile(name, this, content))
+
+
+enum class FileType {
 
 }
-object Attribute{
-    const val None=0
-    const val Hide=1
+
+object FileAttribute {
+    const val None = 0
+    const val Hide = 1
 
 }
--- src/main/kotlin/Path.kt
+++ src/main/kotlin/Path.kt
@@ -1,12 +1,60 @@
-
 @JvmInline
-value class Path(val value: String)
-object PathManager {
-    private var path: Path = Path("/")
-    fun getPath(): Path {
-        return path
+value class Path(val value: String) {
+    fun asAbsolute() {
+
     }
-    fun setPath(path: Path) {
-        this.path = path
+}
+
+object LocationManager {
+    var currentDirectory = FileManager.homeDir
+        private set
+    var currentPath: Path = currentDirectory.getFullPath()
+        private set
+
+
+
+    @JvmName("getterPath")
+    fun getPath(): Path {
+        return currentPath
+    }
+
+
+    fun setPath(path: Path): Boolean {
+        return (tryResolve(path) as? Directory)?.let {
+            currentPath = path
+            currentDirectory = it
+        } != null
+    }
+
+    fun setPath(dir: Directory) {
+        currentPath = dir.getFullPath()
+        currentDirectory = dir
+    }
+
+
+
+    /**
+     * 渡された[path]を解決します
+     * @return 見つからなければnull
+     * */
+    fun tryResolve(path: Path): File? {
+        val isAbsolute = path.value.startsWith("/")
+        val isHomeDir = path.value.startsWith("~")
+        val partialPath = path.value.split("/")
+        return if (isAbsolute || isHomeDir) {
+            partialPath.fold<String, File?>(if (isHomeDir) FileManager.homeDir else root) { dir, partial ->
+                (dir as? Directory)?.children?.get(partial)
+            }
+        } else {
+            partialPath.fold<String, File?>(currentDirectory) { dir, partial ->
+                (dir as? Directory)?.let {
+                    when (partial) {
+                        "." -> it
+                        ".." -> it.parent
+                        else -> it.children[partial]
+                    }
+                }
+            }
+        }
     }
 }
