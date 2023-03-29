f0a14158e6eb35336d1e3b6686ff26e4a7852b04
Naotiki <j22413@inc.kisarazu.ac.jp>
2023-03-29 18:19:21 +0900

    大勝利

--- client-gui/src/main/kotlin/me/naotiki/ese/gui/component/TextLog.kt
+++ client-gui/src/main/kotlin/me/naotiki/ese/gui/component/TextLog.kt
@@ -1,5 +1,7 @@
 package me.naotiki.ese.gui.component
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -10,12 +12,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -24,7 +26,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.yield
 import kotlin.text.Regex.Companion.fromLiteral
 
 val lineSeparator = '\n'
@@ -36,24 +37,25 @@ class TextLogState(maxLineCount: Int) {
     private fun newLine() {
 
         //lastLine+= lineSeparator
-        lines= if (lines.size > maxLineCount) listOf("")+lines.dropLast(1) else listOf("")+lines
-     /*  lines.add(0,"")
-        //Overflow時 TODO ログファイル的なのに書き込む？
-        if (lines.size > maxLineCount) lines.removeAt(lines.lastIndex)*/
+        lines = if (lines.size > maxLineCount) listOf("") + lines.dropLast(1) else listOf("") + lines
+        /*  lines.add(0,"")
+           //Overflow時 TODO ログファイル的なのに書き込む？
+           if (lines.size > maxLineCount) lines.removeAt(lines.lastIndex)*/
     }
+
     private var firstLine
         get() = lines[0]
         set(value) {
-            lines=lines.toMutableList() .apply{ set(0, value) }
+            lines = lines.toMutableList().apply { set(0, value) }
         }
     private var lastLine
         get() = lines[lines.lastIndex]
         set(value) {
-            lines=lines.toMutableList() .apply{ set(lines.lastIndex, value) }
-           // lines[lines.lastIndex] = value
+            lines = lines.toMutableList().apply { set(lines.lastIndex, value) }
+            // lines[lines.lastIndex] = value
         }
 
-    val mutex= Mutex()
+    val mutex = Mutex()
     fun addString(line: CharSequence) {
         arrayOfNulls<String>(50).indexOfLast { it != null }
         val separatedString = line.lines()
@@ -65,7 +67,7 @@ class TextLogState(maxLineCount: Int) {
         }
     }
 
-    suspend fun addChar(char: Char) =mutex.withLock{
+    suspend fun addChar(char: Char) = mutex.withLock {
         if (char == lineSeparator) {
             newLine()
             return
@@ -74,9 +76,9 @@ class TextLogState(maxLineCount: Int) {
     }
 
     fun clear() {
-        lines= listOf("")
+        lines = listOf("")
         //lines.clear()
-       // newLine()
+        // newLine()
     }
 }
 
@@ -85,30 +87,91 @@ fun rememberTextLogState(initialLineCount: Int) = remember {
     TextLogState(initialLineCount)
 }
 
-@Composable
-fun TextLog(state: TextLogState,modifier: Modifier=Modifier, fontSize: TextUnit = TextUnit.Unspecified,) {
-    val lazyListState = rememberLazyListState()
-  /*  val lastItemIndex by remember {
-        derivedStateOf {
-            lazyListState.layoutInfo.totalItemsCount -1
-        }
-    }
-    LaunchedEffect(state.lines){
-     //   lazyListState.scrollToItem(index = state.lines.lastIndex.coerceIn(0,3999))
-    }*/
-    SelectionContainer(Modifier.fillMaxWidth().then(modifier)) {
-        LazyColumn(Modifier.fillMaxWidth(), state = lazyListState, reverseLayout = true) {
-            items(state.lines.size){
-                Text(
-                    state.lines.getOrNull(it)?:"",
-                    Modifier.fillMaxWidth(),
-                    overflow = TextOverflow.Visible,
-                    color = Color.LightGray,
-                    fontSize = fontSize,
+fun calc() {
 
-                    fontFamily = FontFamily.Monospace,
-                    // maxLines = 1
-                )
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun TextLog(
+    state: TextLogState, modifier: Modifier = Modifier, fontSize: TextUnit = TextUnit.Unspecified,
+    letterSpacing: TextUnit = 0.0.em
+) {
+    val lazyListState = rememberLazyListState()
+
+    SelectionContainer(Modifier.fillMaxWidth().then(modifier)) {
+        LazyColumn(Modifier.fillMaxWidth().onSizeChanged {
+
+            /*    val fontPx = with(localDensity) {
+                    fontSize.toPx()
+                }
+                println(fontPx)
+                val spacePx = letterSpacing.value * fontPx
+                println(spacePx)
+                val x = (it.width + spacePx) / (fontPx + spacePx)
+                println(x * 2)*/
+        }, state = lazyListState, reverseLayout = true) {
+            items(state.lines.count()) {
+
+                BoxWithConstraints {
+                    Column(modifier=Modifier.fillParentMaxWidth()) {
+                        val textMeasurer = rememberTextMeasurer(0)
+                        val text = AnnotatedString(state.lines.getOrNull(it) ?: run {
+                            println("LazyList has got null")
+                            ""
+                        })
+
+                        val result = textMeasurer.measure(
+                            text,
+                            overflow = TextOverflow.Visible,
+                            constraints = this@BoxWithConstraints.constraints,
+                            style = TextStyle(
+                                color = Color.LightGray,
+                                fontSize = fontSize,
+                                fontFamily = FontFamily.Monospace,
+                                letterSpacing = letterSpacing,
+                            )
+                        )
+                        repeat(result.lineCount) { lineIndex ->
+                            val start=result.getLineStart(lineIndex)
+                            val end=result.getLineEnd(lineIndex)
+                            Text(
+                                text.subSequence(start,end).plus(AnnotatedString("\n")),
+                                Modifier.fillMaxWidth(),
+                                style = TextStyle(
+                                    color = Color.LightGray,
+                                    fontSize = fontSize,
+                                    fontFamily = FontFamily.Monospace,
+                                    letterSpacing = letterSpacing,
+                                ),
+                                overflow = TextOverflow.Visible,
+                                maxLines = 1
+                            )
+
+                        }
+                    }
+                }
+
+
+                /* Text(
+                     state.lines.getOrNull(it) ?: run {
+                         println("LazyList has got null")
+                         ""
+                     },
+
+                     Modifier.fillMaxWidth(),
+                     overflow = TextOverflow.Visible,
+                     color = Color.LightGray,
+                     fontSize = fontSize,
+                     fontFamily = FontFamily.Monospace,
+                     letterSpacing = letterSpacing,
+
+                     //   maxLines = 1,
+                     onTextLayout = {
+                         println(it.getLineStart(1.coerceIn(0, it.lineCount - 1)))
+                     }
+                 )*/
+
             }
         }
     }
@@ -126,25 +189,29 @@ private fun AnnotatedString.Builder.addStyle(style: SpanStyle, text: String, reg
 }
 
 const val sample = "1234567890123456789012345678901234567890123456789"
-private fun main()= runBlocking {
+
+@OptIn(ExperimentalTextApi::class)
+private fun main() = runBlocking {
     val annotatedString = buildAnnotatedString {
         append(sample)
         addStyle(SpanStyle(Color.Red), sample, "0")
     }
-    AnnotatedString(sample, SpanStyle())
+
+
 
     application {
-        Window(onCloseRequest = {exitApplication()}) {
+        Window(onCloseRequest = { exitApplication() }) {
             MaterialTheme {
                 Surface {
-                    val state= rememberTextLogState(4000)
-                    LaunchedEffect(Unit){
+                    val state = rememberTextLogState(4000)
+                    LaunchedEffect(Unit) {
                         launch {
                             delay(1000)
-                            repeat(1000){
+                            state.addString(sample)
+                            /*repeat(1000) {
                                 state.addString("yes$it\n")
                                 yield()
-                            }
+                            }*/
                         }
                     }
                     TextLog(state, fontSize = 20.sp)
